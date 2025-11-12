80611286edab682d1c8cc909569105702ebd91c8
Jim Walejko <jwalejko@me.com>
2025-11-11 21:08:12 -0700

    Prof. Gallaugher Swift Programming Course - Chapter 1.7, Adding our own images = Asset Catalog, renamed variables all at onvce with refactor > rename.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,46 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
+    //@State private var imageString = ""
     @State private var imageName = ""
     var body: some View {
         
         VStack {
             Spacer()
-            //Image(systemName: "hand.thumbsup")
-            Image(systemName: imageName)
+            
+            //Image("image0")
+            Image(imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
-            // .frame(width: 200, height: 200
-            // )
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+            
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
-                .foregroundStyle(.black)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            
             Spacer()
         
             Button("Press Me!") {
                 let message1 = "You are Awesome!"
                 let message2 = "You are Great!"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
-                
-//                if message == message1{
-//                    message = message2
-//                    imageName = imageString2
-//                }else{
-//                    message = message1
-//                    imageName = imageString1
-//                }
+//                let imageString1 = "image0"
+//                let imageString2 = "image1"
+            
     // Using Ternary Operator"
-                imageName = (message == message1 ? imageString2 : imageString1)
+               // imageName = (message == message1 ? imageString2 : imageString1)
                 message = (message == message1 ? message2 : message1)
-                
+                imageName = (imageName == "image0" ? "image1" : "image0")
             }
             
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
         }
         .padding()
         
