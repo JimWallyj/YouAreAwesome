7b7feeaa9fbc56db2e4efcdac4a67bb3e28793a6
Jim Walejko <jwalejko@me.com>
2025-11-16 20:04:32 -0700

    Prof. Gallaugher Swift Programming Course - Chapter 1.10 = minimumScaleFactor and the SwiftUI animation modifier.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -16,35 +16,26 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
-            
-            //Image("image0")
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 30)
             
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
                 .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                //.animation(.bouncy(duration: 0.5, extraBounce: 0.2), value: imageName)
+                .animation(.easeInOut(duration: 0.15), value: message)
             
             Spacer()
         
             Button("Show Me!") {
-//                let message1 = "You are Awesome!"
-//                let message2 = "You are Great!"
-//                let message3 = "Fabulous? That's you!"
-
-//                if message == message1{
-//                    message = message2
-//                } else if message == message2{
-//                    message = message3
-//                } else{
-//                    message = message1
-//                }
                 
                 let messages = ["You Are Awesome!",
                                 "When the Genius Bar Needs Help, They Call You!",
@@ -53,8 +44,7 @@ struct ContentView: View {
                                 "You Are Fantstic!",
                                 "You Swifty!",
                                 "You Are a Code Monster!",
-                                "You Make Me Smile!",
-                                "Added a new one"]
+                                "You Make Me Smile!"]
                 
                 message = messages[messageNumber]
                 messageNumber += 1
